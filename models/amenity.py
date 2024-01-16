7f74fbe05f86c61321c2a0ab80a06ae5db4c98b5
mouadedd <mdx.edd@gmail.com>
2024-01-16 05:29:24 -0800

    test

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
 
 
 class Amenity(BaseModel):
-    name = ""
+    ''' class for Amenity '''
+    name = Column(String(128), nullable=False)
