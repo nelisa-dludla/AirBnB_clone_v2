fcc8c9f213bb03fb6f85ff0eb589f9e988bc882f
Nelisa <nelisa.dludla@outlook.com>
2024-02-01 00:18:02 +0200

    Added task 0 - 4 files, but only solutions to tasks 0 and 1 are done

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# This script sets up the web servers for the deployment of web_static
+
+nginx_dir="/etc/nginx"
+data_dir="/data/web_static"
+
+if [ ! -e "$nginx_dir" ]; then
+	apt update
+	apt install nginx -y
+fi
+
+mkdir -p "$data_dir/releases/test"
+mkdir -p "$data_dir/shared"
+
+chown ubuntu:ubuntu /data
+
+html_content="<html>
+	<head></head>
+	<body>
+		<h1>Holberton School</h1>
+	</body>
+</html>
+" 
+
+echo "$html_content" > "$data_dir/releases/test/index.html"
+
+ln -sf "$data_dir/releases/test/" "$data_dir/current/"
+
+config_content="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+
+	index index.html index.nginx-debian.html;
+
+	server_name _;
+	add_header X-Served-By \$hostname;
+	
+	location /hbnb_static {
+		alias /data/web_static/current;
+	}
+
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+}
+"
+
+echo "$config_content" > /etc/nginx/sites-available/default
+
+service nginx restart
---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+This script generates a .tgz archive from the
+contents of web_static using Fabric
+'''
+
+from fabric import task, local
+from datetime import datetime
+
+@task
+def do_pack():
+    '''
+    This function compresses the web_static directory
+    '''
+
+    date_obj = datetime.now()
+    year = date_obj.year
+    month = date_obj.strftime('%m')
+    day = date_obj.strftime('%d')
+    hour = date_obj.strftime('%H')
+    minute = date_obj.strftime('%M')
+    second = date_obj.strftime('%S')
+
+    path = f'versions/web_static_{year}{month}{day}{hour}{minute}{second}.tgz'
+
+    local('mkdir -p versions')
+    result = local(f'tar -czvf {path} web_static', capture=True)
+
+    if result.succeeded:
+        return path
+    else:
+        return None
