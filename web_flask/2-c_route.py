ae09b581ca2d8dd3866c7aafc21963bbe62037c9
Nelisa <nelisa.dludla@outlook.com>
2024-02-16 20:36:29 +0200

    Updated all solutions inside web_flask dir to be pycodestyle compliant

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,24 @@
+#!usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
+
 
 if __name__ == '__main__':
     app.run()
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,25 +1,31 @@
+#!usr/bin/python3
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     formatted_text = text.replace('_', ' ')
     return f'C {formatted_text}'
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     formatted_text = text.replace('_', ' ')
     return f'Python {formatted_text}'
+
 
 if __name__ == '__main__':
     app.run()
