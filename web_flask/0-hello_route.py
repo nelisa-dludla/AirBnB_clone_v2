61ce7e4c929dfd87dadee09f008ce5ab917fe3ed
Nelisa <nelisa.dludla@outlook.com>
2024-02-18 22:22:23 +0200

    Updated task 0 in web_flask directory

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,14 @@ This script that starts a Flask web application
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     '''hello route'''
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0')
