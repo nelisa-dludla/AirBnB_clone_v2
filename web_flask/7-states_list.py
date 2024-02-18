06d5ccfb17d741958aecddff304c2d7630f29955
Nelisa <nelisa.dludla@outlook.com>
2024-02-19 00:47:19 +0200

    Added task 7 solution

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This script starts a Flask web application
+'''
+
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_route():
+    ''' states_list route'''
+    states_dict = storage.all()
+    return render_template('7-states_list.html', states=states_dict)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    ''' shuts down the MySQL session '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
